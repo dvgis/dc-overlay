aaef318e8f4be767a815b8e4d38a2b58bbff8bfa
Caven Chen <cavencj@gmail.com>
2020-07-27 10:58:09 +0800

    1.添加代码混淆功能,2.发布2.0.2版本

--- webpack.conf.js
+++ webpack.conf.js
@@ -2,13 +2,14 @@
  * @Author: Caven
  * @Date: 2020-01-18 18:22:23
  * @Last Modified by: Caven
- * @Last Modified time: 2020-05-12 20:16:30
+ * @Last Modified time: 2020-07-27 10:47:43
  */
 
 const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const JavaScriptObfuscator = require('webpack-obfuscator')
 
 function resolve(dir) {
   return path.join(__dirname, '.', dir)
@@ -26,6 +27,14 @@ module.exports = env => {
   if (IS_PROD) {
     plugins.push(new OptimizeCssAssetsPlugin())
     plugins.push(new webpack.NoEmitOnErrorsPlugin())
+    plugins.push(
+      new JavaScriptObfuscator(
+        {
+          rotateStringArray: true
+        },
+        []
+      )
+    )
   }
   return {
     entry: {
